1e03b77b64ccf1be27edeaa4ee29d4d1f8b20f30
blossomou <qingqingou@gmail.com>
2022-01-15 16:16:17 -0500

    Adding Gravity to the world

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { GameEngine } from 'react-native-game-engine';
 import entities from './entities';
+import Physics from './physics';
+
 
 export default function App() {
+  const [running, setRunning] = useState(false);
+  useEffect(() => {
+    setRunning(true);
+  },[])
   return (
     <View style={{flex: 1}}>
-      <GameEngine entities={entities()}
+      <GameEngine 
+        systems={[Physics]}
+        entities={entities()}
+        running={running}
         style={{position: 'absolute',top: 0, left: 0, right: 0, bottom: 0}}
       >
 
