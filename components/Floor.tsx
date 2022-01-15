aa4247a3ae7528998830cc8bb39622064e55271c
blossomou <qingqingou@gmail.com>
2022-01-15 16:37:49 -0500

    Add a floor component

---
+++ components/Floor.tsx
@@ -0,0 +1,51 @@
+import { Bodies, World } from 'matter-js'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+// interface pos {
+//     x: number,
+//     y: number;
+// }
+
+const Floor = (props : any) => {
+    const widthBody = props.body.bounds.max.x - props.body.bounds.min.x;
+    const heightBody = props.body.bounds.max.y - props.body.bounds.min.y;
+
+    const xBody = props.body.position.x - widthBody/2;
+    const yBody = props.body.position.y - heightBody/2;
+
+    const color = props.color;
+
+    return(
+        <View style={{
+            backgroundColor: color,
+            position: 'absolute', left: xBody, top: yBody, width: widthBody, height: heightBody}}
+        />
+        
+    )
+}
+
+export default function (world:World, color: string, pos: {x: number, y: number}, size: {width: number, height: number}) {
+    const initialFloor = Bodies.rectangle(
+        pos.x,
+        pos.y,
+        size.width,
+        size.height,
+        {
+           label: 'Floor',
+           isStatic: true
+
+        } 
+    )
+
+    World.add(world, initialFloor);
+
+    return {
+        body: initialFloor,
+        color,
+        pos,
+        renderer: <Floor/>
+    }
+}
+
+
--- entities/index.ts
+++ entities/index.ts
@@ -1,5 +1,12 @@
 import Matter from 'matter-js'
 import Bird from '../components/Bird';
+import Floor from '../components/Floor';
+import { Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+
 
  const restart = () => {
     let engine = Matter.Engine.create({enableSleeping: false, gravity: {y:0.4}})
@@ -7,7 +14,9 @@ import Bird from '../components/Bird';
  
     return{
         physics: {engine, world},
-        Bird: Bird(world, 'green', {x: 50, y: 300}, {height: 40, width:40})
+        Bird: Bird(world, 'green', {x: 50, y: 300}, {height: 40, width:40}),
+        Floor: Floor(world, 'green', {x: windowWidth/2, y: windowHeight}, {height: 50, width:windowWidth})
+
 
     }
 }
