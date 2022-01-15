174b7f5a117dc7b13812b94eebd9a0d61b8e5b00
blossomou <qingqingou@gmail.com>
2022-01-15 17:23:40 -0500

    Adding Movement to the Obstacles

--- entities/index.ts
+++ entities/index.ts
@@ -15,6 +15,8 @@ const windowHeight = Dimensions.get('window').height;
     let engine = Matter.Engine.create({enableSleeping: false, gravity: {y:0.4}})
     let world = engine.world;
     const pipeSizePosA = getPipeSizePosPair()
+    const pipeSizePosB = getPipeSizePosPair( windowWidth * 0.9)
+
     return{
         physics: {engine, world},
         
@@ -22,6 +24,9 @@ const windowHeight = Dimensions.get('window').height;
         
         ObstacleTop1: Obstacle(world, 'ObstacleTop1', 'red', pipeSizePosA.pipeTop.pos, pipeSizePosA.pipeTop.size),
         ObstacleBottom1: Obstacle(world, 'ObstacleBottom1', 'blue', pipeSizePosA.pipeBottom.pos, pipeSizePosA.pipeBottom.size),
+
+        ObstacleTop2: Obstacle(world, 'ObstacleTop2', 'red', pipeSizePosB.pipeTop.pos, pipeSizePosB.pipeTop.size),
+        ObstacleBottom2: Obstacle(world, 'ObstacleBottom2', 'blue', pipeSizePosB.pipeBottom.pos, pipeSizePosB.pipeBottom.size),
 
         Floor: Floor(world, 'green', {x: windowWidth/2, y: windowHeight}, {height: 50, width:windowWidth})
 
--- physics.ts
+++ physics.ts
@@ -1,4 +1,11 @@
 import Matter from "matter-js";
+import { getPipeSizePosPair } from "./utils/random";
+
+
+import { Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const Physics= (entities: { physics: { engine: any; }; }, {touches, time, dispatch}: any) =>{
     let engine = entities.physics.engine;
@@ -11,6 +18,17 @@ const Physics= (entities: { physics: { engine: any; }; }, {touches, time, dispat
     })
     
     Matter.Engine.update(engine, time.delta);
+
+    for(let i = 1; i <= 2; i++){
+        if(entities[`ObstacleTop${i}`].body.bounds.max.x <= 0 ){
+            const pipeSizePos = getPipeSizePosPair(windowWidth * 0.9);
+            Matter.Body.setPosition(entities[`ObstacleTop${i}`].body, pipeSizePos.pipeTop.pos)
+            Matter.Body.setPosition(entities[`ObstacleBottom${i}`].body, pipeSizePos.pipeBottom.pos)
+        }
+        Matter.Body.translate(entities[`ObstacleTrop${i}`].body, {x: -3, y:0})
+        Matter.Body.translate(entities[`ObstacleBottom${i}`].body, {x: -3, y:0})
+    }
+
 
     return entities;
 
